de424cf01f7464977c437eceb95987d960889957
fThauana <fthauanavitoria@gmail.com>
2025-06-15 20:01:43 -0300

    sem commit

--- area_professor-main/src/controller/MenuController.java
+++ area_professor-main/src/controller/MenuController.java
@@ -5,12 +5,11 @@ import java.util.Scanner;
 public class MenuController {
     static Scanner sc = new Scanner(System.in);
 
-    // Método utilitário para ler um índice válido de lista
     public static int lerIndiceValido(int tamanhoLista, Scanner sc) {
     while (true) {
         try {
             String entrada = sc.nextLine();
-            int idx = Integer.parseInt(entrada) - 1; // Usuário vê de 1 em 1, lista é 0-based
+            int idx = Integer.parseInt(entrada) - 1; 
 
             if (idx >= 0 && idx < tamanhoLista) {
                 return idx;
